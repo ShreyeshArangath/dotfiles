eb3f2f16cd80f6ef93398322e861438d5f3e14e2
ShreyeshArangath <shreyesh.arangath@gmail.com>
2026-04-14 14:14:17 -0700

    Add custom keymaps and enable snacks picker/explorer extras
    - sf: search files, ff: fuzzy find (grep)
    - Enable snacks_picker and snacks_explorer LazyVim extras
    - Fix tmux splits to preserve current path on default bindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "sf", function() Snacks.picker.files() end, { desc = "Search Files" })
+vim.keymap.set("n", "ff", function() Snacks.picker.grep() end, { desc = "Fuzzy Find (grep)" })
